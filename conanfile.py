a0c43f678964e148d2463680c914c3c025c03a0c
sideshowbobgot <sideshowbobgot@gmail.com>
2024-03-30 01:56:56 +0200

    changed version

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 
 class TypeSafe(ConanFile):
     name = 'type_safe'
-    version = '1.0.1'
+    version = '1.0.2'
     user = "user"
     channel = "stable"
     settings = ("os", "build_type", "arch", "compiler")
