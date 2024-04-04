390f71d61a84e00da2e0558e4733c9e04f85d710
sideshowbobgot <sideshowbobgot@gmail.com>
2024-04-04 15:13:36 +0300

    added math funcs to std

--- example/type_cast.cpp
+++ example/type_cast.cpp
@@ -14,6 +14,12 @@ int main() {
 	type_safe::types::uint32_t b = 255u;
 	static_assert(type_safe::detail::is_safe_integer_conversion<uint16_t, uint32_t>::value);
 	c * a;
-	std::cout << (a < b) << std::endl;
-	std::cout << (b > a) << std::endl;
+	// std::cout << (a < b) << std::endl;
+	// std::cout << (b > a) << std::endl;
+
+	type_safe::float_t f1 = 4.f;
+	type_safe::float_t f2 = 2.f;
+	std::cout << std::pow(f1, f2) << "\n";
+	std::cout << std::pow(f1, 2.f) << "\n";
+	std::cout << std::pow(4.f, f2) << "\n";
 }
--- include/type_safe/math.hpp
+++ include/type_safe/math.hpp
@@ -5,66 +5,93 @@
 #include <cmath>
 #include <algorithm>
 
-namespace type_safe {
-	namespace math {
-		template<typename IntegerType, typename Policy>
-		TYPE_SAFE_FORCE_INLINE constexpr auto abs(const integer<IntegerType, Policy>& value) noexcept ->
-			integer<IntegerType, Policy> {
-			return static_cast<IntegerType>(std::abs(static_cast<IntegerType>(value)));
-		}
+namespace std {
+	template<typename IntegerType, typename Policy>
+	TYPE_SAFE_FORCE_INLINE constexpr auto abs(const type_safe::integer<IntegerType, Policy>& value) noexcept ->
+	type_safe::integer<IntegerType, Policy> {
+		return static_cast<IntegerType>(std::abs(static_cast<IntegerType>(value)));
+	}
 
-		#define FLOAT_OPERATION(func_name)\
+	#define FLOAT_OPERATION(func_name)\
 			template<typename FloatingType>\
-			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(const floating_point<FloatingType>& value) noexcept ->\
-				floating_point<FloatingType> {\
+			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(const type_safe::floating_point<FloatingType>& value) noexcept ->\
+				type_safe::floating_point<FloatingType> {\
 				return static_cast<FloatingType>(std::func_name(static_cast<FloatingType>(value)));\
 			}
 
-			FLOAT_OPERATION(abs);
-			FLOAT_OPERATION(sin);
-			FLOAT_OPERATION(cos);
-			FLOAT_OPERATION(tan);
-			FLOAT_OPERATION(asin);
-			FLOAT_OPERATION(acos);
-			FLOAT_OPERATION(atan);
-			FLOAT_OPERATION(sinh);
-			FLOAT_OPERATION(cosh);
-			FLOAT_OPERATION(tanh);
-			FLOAT_OPERATION(asinh);
-			FLOAT_OPERATION(acosh);
-			FLOAT_OPERATION(atanh);
+		FLOAT_OPERATION(abs);
+		FLOAT_OPERATION(floor);
+		FLOAT_OPERATION(ceil);
+		FLOAT_OPERATION(exp);
+		FLOAT_OPERATION(log);
+		FLOAT_OPERATION(log10);
+		FLOAT_OPERATION(sqrt);
+		FLOAT_OPERATION(sin);
+		FLOAT_OPERATION(cos);
+		FLOAT_OPERATION(tan);
+		FLOAT_OPERATION(asin);
+		FLOAT_OPERATION(acos);
+		FLOAT_OPERATION(atan);
+		FLOAT_OPERATION(sinh);
+		FLOAT_OPERATION(cosh);
+		FLOAT_OPERATION(tanh);
+		FLOAT_OPERATION(asinh);
+		FLOAT_OPERATION(acosh);
+		FLOAT_OPERATION(atanh);
+		FLOAT_OPERATION(round);
 
-		#undef FLOAT_OPERATION
+	#undef FLOAT_OPERATION
 
-		template<typename FloatingType>
-		TYPE_SAFE_FORCE_INLINE constexpr auto atan2(
-			const floating_point<FloatingType>& y,
-			const floating_point<FloatingType>& x
-		) noexcept -> floating_point<FloatingType> {
-			return static_cast<FloatingType>(std::atan2(static_cast<FloatingType>(y), static_cast<FloatingType>(x)));
-		}
 
-		#define NUMBER_PAIR_OPERATION(func_name)\
+	#define FLOAT_PAIR_OPERATION(func_name)\
+			template<typename FloatingType>\
+			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(\
+				const type_safe::floating_point<FloatingType>& y,\
+				const type_safe::floating_point<FloatingType>& x\
+			) noexcept -> type_safe::floating_point<FloatingType> {\
+				return static_cast<FloatingType>(std::func_name(static_cast<FloatingType>(y), static_cast<FloatingType>(x)));\
+			}                                    \
+			template<typename FloatingType>\
+			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(\
+				const type_safe::floating_point<FloatingType>& y,\
+				const FloatingType& x\
+			) noexcept -> type_safe::floating_point<FloatingType> {\
+				return static_cast<FloatingType>(std::func_name(static_cast<FloatingType>(y), static_cast<FloatingType>(x)));\
+			}                                    \
+			template<typename FloatingType>\
+			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(\
+				const FloatingType y,\
+				const type_safe::floating_point<FloatingType>& x\
+			) noexcept -> type_safe::floating_point<FloatingType> {\
+				return static_cast<FloatingType>(std::func_name(static_cast<FloatingType>(y), static_cast<FloatingType>(x)));\
+			}
+
+		FLOAT_PAIR_OPERATION(atan2);
+		FLOAT_PAIR_OPERATION(pow);
+		FLOAT_PAIR_OPERATION(fmod);
+		FLOAT_PAIR_OPERATION(copysign);
+	#undef FLOAT_PAIR_OPERATION
+
+	#define NUMBER_PAIR_OPERATION(func_name)\
 			template<typename IntegerType, typename Policy>\
 			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(\
-				const integer<IntegerType, Policy>& first,\
-				const integer<IntegerType, Policy>& second\
-			) noexcept -> integer<IntegerType, Policy> {\
+				const type_safe::integer<IntegerType, Policy>& first,\
+				const type_safe::integer<IntegerType, Policy>& second\
+			) noexcept -> type_safe::integer<IntegerType, Policy> {\
 				return static_cast<IntegerType>(std::func_name(static_cast<IntegerType>(first), static_cast<IntegerType>(second)));\
 			}\
 			template<typename FloatingType>\
 			TYPE_SAFE_FORCE_INLINE constexpr auto func_name(\
-				const floating_point<FloatingType>& first,\
-				const floating_point<FloatingType>& second\
-			) noexcept -> floating_point<FloatingType> {\
+				const type_safe::floating_point<FloatingType>& first,\
+				const type_safe::floating_point<FloatingType>& second\
+			) noexcept -> type_safe::floating_point<FloatingType> {\
 				return static_cast<FloatingType>(std::func_name(static_cast<FloatingType>(first), static_cast<FloatingType>(second)));\
 			}
 
-			NUMBER_PAIR_OPERATION(max);
-			NUMBER_PAIR_OPERATION(min);
-		#undef NUMBER_PAIR_OPERATION
+		NUMBER_PAIR_OPERATION(max);
+		NUMBER_PAIR_OPERATION(min);
+	#undef NUMBER_PAIR_OPERATION
 
-	}
 }
 
 #endif // TYPE_SAFE_MATH_HPP_INCLUDED
